460128ea5ec34415c85eae58e331f640b8bff8c5
MATÍAS GARCÍA GIMENO <mgg709@inlumine.ual.es>
2022-06-01 11:55:06 +0200

    subida completa sin opcional

--- SeleniumHMIS-mgg709/test/org/ual/hmis/equipo7/DetallesCorrectoTest.java
+++ SeleniumHMIS-mgg709/test/org/ual/hmis/equipo7/DetallesCorrectoTest.java
@@ -34,7 +34,7 @@ public class DetallesCorrectoTest {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-		//System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		ChromeOptions chromeOptions = new ChromeOptions(); 
 	    chromeOptions.setHeadless(true);
--- SeleniumHMIS-mgg709/test/org/ual/hmis/equipo7/EliminacionTest.java
+++ SeleniumHMIS-mgg709/test/org/ual/hmis/equipo7/EliminacionTest.java
@@ -34,7 +34,7 @@ public class EliminacionTest {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-		//System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		ChromeOptions chromeOptions = new ChromeOptions(); 
 	    chromeOptions.setHeadless(true);
